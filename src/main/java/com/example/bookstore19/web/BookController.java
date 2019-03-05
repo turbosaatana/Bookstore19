9a41d7f901f401a2b29751f013b05f1d30af778e
turbosaatana <44897108+turbosaatana@users.noreply.github.com>
2019-03-05 21:12:32 +0200

    added testi

--- src/main/java/com/example/bookstore19/web/BookController.java
+++ src/main/java/com/example/bookstore19/web/BookController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class BookController {
     @Autowired
     private BookRepository repository;
-
+//testitesti
     @RequestMapping("/booklist")
     public String bookList(Model model){
         model.addAttribute("books", repository.findAll());
